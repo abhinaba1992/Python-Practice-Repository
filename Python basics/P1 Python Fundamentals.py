101d17b4b146e426375825833da6a92640a11839
Abhinaba Chakraborty <abhinaba.1992@gmail.com>
2018-01-21 00:25:00 +0530

    Update P1 Python Fundamentals.py

--- Python basics/P1 Python Fundamentals.py
+++ Python basics/P1 Python Fundamentals.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  7 07:47:57 2018
+
+@author: Abhinaba
+"""
 #Pyhton part 1 training 24th Dec 2017
 #Author: Abhinaba Chakraborty
 #Date: 19th Jan 2018
@@ -23,27 +29,27 @@ y="data science"
 
 #IMPORTING LIBRARIES
 #We can import a library by using the keyword "import" (NOTE: Keyword is an actual library here)
-#e.g. 1
+#e.g. 1 [This has to be ran from console located down right]
 import Keyword #for importing a library named Keyword
 #Now if we wish to import specific components from the library, we can do
-Keyword.Kwlist
+Keyword.Kwlist #[This has to be ran from console located down right]
 
 #e.g. 2
 #We can also import parts of a library in the following way
-from Keyword import Kwlist
+from Keyword import Kwlist #[This has to be ran from console located down right]
 #Now the above approach is useful when we do not have another component with the same name in another library, like for e.g. we cannot have
 #"from Keyword import Kwlist" and "from <some other library> import Kwlist" in the same piece of code as it will cause problem, so its always
 #a good practice to import the library first and then refer to specific portions of the same like shown in example 1
 
 #e.g. 3
-from Keyword import *
+from Keyword import * #[This has to be ran from console located down right]
 #This is very much similar to e.g. 1, however, the only difference is that in example 1 you have to refer to specific components by preceding
 #them with the name of the library and a dot(e.g. Keyword.Kwlist), but in e.g. 3 you can directly include the components with the component 
 #name(Kwlist)
 
 #e.g. 4
-import Keyword as kw
-kw.Kwlist
+import Keyword as kw #[This has to be ran from console located down right]
+kw.Kwlist  #[This has to be ran from console located down right]
 
 #VARIABLE TYPES
 #string, integer and float
@@ -80,7 +86,7 @@ x=float(x) # casting and assigning it back to the main variable x
 
 
 #GETTING SIZES OF VARIABLES
-from sys import sizeof
+import sys #[This has to be ran from console located down right]
 x=1
 sys.getsizeof(x) #Gives us the size occupied by the variable in the memory in bits
 
@@ -105,3 +111,110 @@ x+2,y+3,x+y,x**(x+y)
 #If we do the following, it would say tuple
 type((x+2,y+3,x+y,x**(x+y))) #We would get a tuple by running this
 #Now the above is particularly useful when we wish to return multiple values from a single return statement
+
+#We can also do something like
+a,b,c,d=x+2,y+3,x+y,x**(x+y)
+#In the above case a would be assigned with x+2, b with y+3, c with x+y, d with x**(x+y)
+
+#Multiple assignments can also be done like
+a,b,c,d=2,3,4,5
+#however, the following would give an error because integer is not an iterable operator. In other words,
+#the assignment operation expects multiple values for assigning, and it will give an error if it's just
+#a single value
+a,b,c=2
+
+#On the contrary, we can do something like the following
+a,b,c=[2,2,2] #However in this case, the values that are being assigned won't be considered as a tuple
+              #but rather as a list of integer values
+              
+#So in a practical scenario we can write a function like the following
+a,b,c,d= f(x,y) return x+2,y+3,x+y,x**(x+y) #This is just an example, original syntax has not been followed
+#So it would create a function that takes in x and y as params and retunrs the values in chronological order
+
+
+#In Python, x^Y doesn't work for exponential values, we need to write x**y,that is
+x^y #is wrong and would give an error
+x**y #is right
+
+
+#In general for bracket related arithmatic evaluation, we use BODMAS rule(Bracket of Diviion, multiplication,
+#addition and substraction)
+#However in python we use a similar concept known as PEDMAS (parenthesis,Exponent,Multiplication,
+#Division(module peration),Addition,Substraction)
+
+z=(x+y)**x-y/x # eqn. within the bracket will be solved first, and then the exponentail part, followed by 
+z              # multiplication part, then the division part and finally the substraction part.So, we will
+               # get a value of z as 25.1621..
+               
+
+#Modular operations
+x=5
+y=2               
+x%y               
+
+
+#Logical operations
+x=True
+y=False
+
+#The follwoing two would give us datatype of the above variables
+type(x)
+type(y)
+
+#Boolean operations (the simpel AND and OR functions)
+x and y #FALSE
+x or y #TRUE
+
+#The AND operation can also be written as
+x & y
+#While the OR operation can also be written as
+x | y
+
+
+#MATH FUNCTION AND ITS OPERATIONS             
+#importing the math library for performing various mathematical functions (note that we can also use numpy 
+#library instead of math library as it's much more generalised and has a wider application
+                    
+import math #[This has to be ran from console located down right]
+x=2
+math.log(x) #Gives us the log of the variable
+math.sin(x) #Gives us the Sin of the variable
+
+#Not of x
+not x
+#However, the following would give an error as this isn't possible in Python
+!x     
+
+#STRING VALUES AND OPERATIONS
+#just like R, both single quotes and double quotes can be used for representing the same
+x='Mumbai'
+y="Bangalore"
+x,y
+
+#Finding the length of a variable in Python
+len(x) #would give 6
+len(y) #would give 9
+x='Mumbai '
+len(x) #This would give 7 as it includes the sapce
+
+#We can also try some special characters for our reference
+x='Mumbai \t\n'
+len(x) #This would give 9, even after appearing to give 11 as output as \t and \n are special characters here
+
+#We can also return a tuple by calling multiple function seperated by a comma in a single line
+x='Mumbai'
+y="Bangalore"
+len(x),len(y)
+type((len(x),len(y))) #This would give us a tuple
+
+
+#String appending
+x+y
+y+x
+
+#The following would give an error
+x-y #As we cannot deduct a string from another string
+
+#Comparing strings with the double equal to operator
+x+y == y+x #Note that this will be false as MumbaiBangalore is not equal to BangaloreMumbai
+
