e4fddec8dadebd74fc50d4528611b692e9cc1444
Abhinaba Chakraborty <abhinaba.1992@gmail.com>
2018-03-13 22:00:42 +0530

    Update P3 Python Fundamentals.py

--- Python basics/P3 Python Fundamentals.py
+++ Python basics/P3 Python Fundamentals.py
@@ -52,6 +52,10 @@ b=np.array([0,2,0,1])
 c=np.arange(4) #This is another way of defining an array using numpy, this is almost like a for loop,
                #the only difference is that it doesn't return a list/range, but a numpy array itself
 c
+
+#Also, in case we want to define a range for arnage, we can also do that, so for e.g., if we do
+c=np.arange(0,4,1) #Start at 0, go for 4 elements, increment by 1
+c
 #Now, if we do a[c], then we would basically get the same result as simply running a because since c 
 #contain [0,1,2,3], running a[c] would mean runing a[0,1,2,3], that would return all the four rows and
 #all cols, so we would bascially get the same result
@@ -64,4 +68,135 @@ a[:4]
 a[c,b] #Here all the rows are selected, however specific cols are selected from each row
 
 a[:4,b] #Similiar ways of achieving the above
+
+
+#Other example definitions with np.arange
+#e.g. 1 (We create an array in this step)
+n=np.array(np.arange(5))
+n
+#Checkout the shape
+n.shape
+
+#e.g. 2 (We create a matrix in this step)
+n=np.matrix(np.arange(5))
+n
+#Checkout the shape
+n.shape 
+#Numpy matrix can only have 2 dimensions, while numpy array can have as many dimensions as possible
+
+#Changing values in a numpy array
+a[c,b] += 10 #Adding 10 to the selected elements (We can also perform actions like multiplication, division 
+a             #etc)
+
+
+
+#Conditional subsetting in numpy arrays
+a=np.array([[1,2],[3,4],[5,6]])
+#Subsetting with a condition
+c=a>2 #c will have a array of the same shape as a and would contain TrueFalse values
+#seeing c value
+c
+
+#Subsetting a only when cases are True (based on the above example)
+a[c] #We would only get those values where c is true
+#or
+a[a>2]
+
+
+a[c].shape #this will givve us 4 since we got 4 TRUEs
+
+
+#More complex subsetting ops.
+a[(a>2) | (a<5)] #or operation
+a[(a>2) & (a<5)] #and operation
+
+
+#Array operations
+#it's important to note that the type of input for a method/operation in a particular library can be either 
+#a numpy array or a numpy matrix depending upon how it's developed (so whatever representation is expected 
+#by our library, we have to put or data in that format)
+
+#Defining 2 arrays, X and Y
+X=np.array([[1,2],[3,4]])
+Y=np.array([[5,6],[7,8]])
+
+#Addition
+X+Y
+#We can also use numpy add function to do the same
+np.add(X,Y)
+
+#Substract
+X-Y
+#using numpy function
+np.subtract(X,Y)
+
+#Multiplication
+X*Y
+#Using numpy function
+np.multiply(X,Y)
+
+#Division
+X/Y
+#Using numpy function
+np.divide(X,Y)
+
+#Square root
+np.sqrt(X)
+#note that numpy is derived from math, so if we do not import numpy, then we cannot perform these operations
+#on numpy array. math library allows performing sqrt on int or float values, however in order to do the same
+#on data structures like numpy arrays, we need the library numpy (implicitly math is a supeclass for numpy)
+
+
+#matrix multiplication using dot function
+V=np.array([9,10])
+W=np.array([11,12])
+
+#Using the dot operator below
+V.dot(W) #We would get 219 ((9*11) + (10*12)
+
+#OR
+V=np.array([[7,8],[9,10]])
+W=np.array([[11,12],[13,14]])
+
+V.dot(W) #We would get 219 ((9*11) + (10*12)
+
+
+#We cannot use dot operator for matrix mul. in case the number of elements in a row is not equal to the equi.
+#column of the matrix
+V=np.array([9,10])
+W=np.array([11])
+
+V.dot(W) #We would get an error while trying to this.
+#Or
+V=np.array([[7,8],[9,10]])
+W=np.array([[11,12],[13,14],[15,16]])
+
+V.dot(W) #We would get an error
+
+
+#Transposing a matrix
+V=np.array([[7,8],[9,10]])
+W=np.array([[11,12],[13,14],[15,16]])
+#Transposing
+V.T
+W.T
+
+#So, the use of the transpose here is that in order to make sure that the dot product works, sometimes we may
+#need to make sure that the matrix is transposed in such a way that the no. of elements in a column matches
+#that of the rows in another matrix
+
+
+#Summing up the elements in a numpy array
+np.sum(X) #WE would get 10
+
+#Summing across dimensions
+np.sum(X,axis=0) #Across cols
+np.sum(X,axis=1) #Across rows
+
+
+#Creating copies of a elements in an array using tile function (It is basically used for copying a similar
+#structure across many number of rows or cols)
+v=([1,0,1]) #Defining an array V
+np.tile(v,(4,1)) #Creating 4 copies having the same elements
+np.tile(v,(4,2)) #Creating 4 copies having twice the number of elements
 
