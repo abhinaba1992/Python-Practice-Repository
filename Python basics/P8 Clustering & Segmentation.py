2b14ade91225406789e79c552e2d763a3f4c5e3f
Abhinaba Chakraborty <abhinaba.1992@gmail.com>
2018-05-24 13:38:42 +0530

    Update P8 Clustering & Segmentation.py

--- Python basics/P8 Clustering & Segmentation.py
+++ Python basics/P8 Clustering & Segmentation.py
@@ -51,6 +51,8 @@ wine_std.describe()
 #assigning a vector
 range_n_clusters=[2,3,4,5,6,7,8,9]
 
+#We would be doing clustering operation based on silhouette_score thorugh K-means algo
+
 
 #In the code below, we are trying different values of cluster numbers and for each we are examining silhoutte 
 #scores along with their silhoutte plots and visual representation of clusters
@@ -139,7 +141,7 @@ for n_clusters in range_n_clusters:
                   "with n_clusters = %d" % n_clusters),
                  fontsize=14, fontweight='bold')
 
-    plt.show()
+#plt.show()
 
 
 #So, from the above appraoch we can infer that the optimum number of cluster must be 3 as after the third
@@ -174,5 +176,155 @@ for i in range(k):
     # plot the data observations
     plt.plot(ds[:,0],ds[:,1],'o')
 
-plt.show()
+#plt.show()
  
+
+
+#We would now be doing the clustering operation using WSS or within sum of squares for with k-means algo
+#(here we would be using the elbow chart)
+
+#WSS is named as inertia_ as kmeansfunction's attribute
+kmeans.inertia_
+#We would get an inertia_ value of 1261.2973748761683
+
+
+#We need to look for inertia/WSS value for many K's and see if our conclusions match with what we have got
+#from the silhoutte score
+Ks=np.linspace(2,15,14)
+
+Ks
+
+#We are now calcualting the WSS so as to plot the elbow chart for understanding the optimum number of clusters
+ssw=[]
+for k in Ks:
+    kmeans=KMeans(n_clusters=int(k))
+    kmeans.fit(wine_std)
+    ssw.append(kmeans.inertia_)
+plt.plot(Ks,ssw)
+
+
+
+
+#hierarchical/agglomaeration clustering
+from sklearn.cluster import AgglomerativeClustering
+
+
+
+for n_clusters in range(2,10):
+    cluster_model = AgglomerativeClustering(n_clusters=n_clusters, affinity='euclidean',linkage='ward')
+    cluster_labels = cluster_model.fit_predict(X)
+    silhouette_avg = silhouette_score(X,cluster_labels,metric='euclidean')
+    print("For n_clusters =", n_clusters, 
+          "The average silhouette_score is:", silhouette_avg)
+
+#We get the best average silhouette score for a k value of 3 which is 3 clusters
+
+
+#Building the clustering model with 3 clusters
+
+s = 3
+hclust = AgglomerativeClustering(n_clusters=s, affinity='euclidean',linkage='ward')
+hclust.fit(wine_std)
+
+
+#Assigning labels to the clustering groups
+labels = hclust.fit_predict(X)
+wine_std["cluster"]=labels
+
+
+#Plotting the same in the graph
+for i in range(s):
+    # select only data observations with cluster label == i
+    hc = wine_std[wine_std["cluster"]==i].as_matrix()
+    # plot the data observations
+    plt.plot(hc[:,0],hc[:,1],'o')
+
+    
+    
+#DBSCAN
+from sklearn.datasets import make_moons
+#We import the data set moons
+
+mydata = make_moons(n_samples = 2000,noise=0.05)
+print(mydata[0].shape)
+mydata=pd.DataFrame(mydata[0],columns=["X","Y"])
+mydata.head()
+
+    
+    
+#Plotting a scattered plot using my data
+from ggplot import *
+
+
+ggplot(mydata,aes(x='X',y='Y'))+geom_point()
+
+
+
+#Running the k means algorithm with k=2 and plotting the same
+kmeans=KMeans(n_clusters=2)
+kmeans.fit(mydata)
+mydata["cluster"]=kmeans.labels_
+ggplot(mydata,aes(x='X',y='Y',color='cluster'))+geom_point()
+
+
+
+#Running the k means algorithm with k=5 and plotting the same
+kmeans=KMeans(n_clusters=5)
+kmeans.fit(mydata)
+mydata["cluster"]=kmeans.labels_
+ggplot(mydata,aes(x='X',y='Y',color='cluster'))+geom_point()
+
+#You can force k-means to make as many datagroups in the data as you want
+#but it wont bother about how many natural groups are there in the data
+#Lets see how DB scan treat this
+
+
+#We are now trying the DB SCAN here
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+
+
+del mydata['cluster']
+
+
+
+db = DBSCAN(eps=0.2, min_samples=10, metric='euclidean').fit(mydata)
+mydata['cluster']=db.labels_
+ggplot(mydata,aes(x='X',y='Y',color='cluster'))+geom_point()
+
+#We can play around with the epsilon and minimim samples
+
+
+del mydata['cluster']
+db = DBSCAN(eps=0.3, min_samples=10, metric='euclidean').fit(mydata)
+mydata['cluster']=db.labels_
+ggplot(mydata,aes(x='X',y='Y',color='cluster'))+geom_point()
+
+
+
+
+#Other examples (extra)
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
+
+
+X = StandardScaler().fit_transform(X)
+
+db = DBSCAN(eps=0.3, min_samples=10).fit(X)
+labels = db.labels_
+
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+print('Estimated number of clusters: %d' % n_clusters_)
+print("Silhouette Coefficient: %0.3f"
+      % metrics.silhouette_score(X, labels))
+
+
+
+df=pd.DataFrame(X,columns=['x1','x2'])
+df['labels']=labels
+
+
+
+ggplot(df,aes(x='x1',y='x2',color='labels'))+geom_point()
